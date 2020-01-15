ff4f79a009469000b72f600a9672c90406f384b6
Jessica Wolff <jleahwolff@gmail.com>
2020-01-15 09:40:56 -0600

    Working thru App.js

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import Card from './components/GitCard';
+import Search from './components/SearchForm';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  super();
+  this.state = {
+    user: [],
+    followers: []
+  }
+  
+  componentDidMount() {
+    axios
+    .get('https://api.github.com/users/jleahwolff')
+    .then( res => {
+      console.log(res, `data`);
+      this.setState({
+        user: res.data
+      })
+    })
+    .catch (err => console.log(err));
+    
+    axios
+    .get('https://api.github.com/users/jleahwolff/followers')
+    .then (res => {
+      console.log(res.data, `followers`);
+      this.setState({
+        followers: res.data
+      });
+    })
+    .catch(err => console.log(err));
+  }
+  
+  
+  
+  function App() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,12 +48,13 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
       </header>
     </div>
   );
+};
 }
 
 export default App;
